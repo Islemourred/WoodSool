2ecc3f4716f3b3b2e5c236677064383f9b231244
Ourred Islem Charaf Eddine <132939084+Islemourred@users.noreply.github.com>
2025-07-01 16:14:05 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist/client", // Ensure this is set correctly
-  },
-  resolve: {
-    alias: {
-      // eslint-disable-next-line no-undef
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  base: './',
+  base: process.env.VITE_BASE_PATH || "/My_Portfolio",
 });
